a00d13decb4ed8cf89d97c2c5e0c348f9046ece0
Juan Fretes <fretesjuan4@gmail.com>
2023-06-25 11:51:10 -0400

    Refactor background.js and manifest.json
    - Simplify the conditions to a single line of code in background.js.
    - Refactor the functionToAddButton() to add a 3 sec delay before it runs.
    - Update permissions in the manifest.json file.
    
    These changes simplify and improve the functionality of the code.

--- background.js
+++ background.js
@@ -1,37 +1,53 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.active && tab.url.includes('https://www.youtube.com/watch?v=')) {
-        console.log("url: ", tab.url)
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            function: functionToAddButton
-        });
-    }
+  if (
+    changeInfo.status === "complete" &&
+    tab.active &&
+    tab.url.includes("https://www.youtube.com/watch?v=")
+  ) {
+    console.log("url: ", tab.url);
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      function: functionToAddButton
+    });
+  }
 });
 
 function functionToAddButton() {
-    let btn = document.createElement('button');
+  setTimeout(() => {
+    let btn = document.createElement("button");
 
     btn.style.borderRadius = "16px";
     btn.style.backgroundColor = "red";
     btn.style.color = "white";
     btn.style.border = "none";
 
-    btn.innerText = 'Open in YouTube Music';
-    btn.setAttribute('id', 'jda-music-button')
-    btn.addEventListener('click', function () {
-        window.open(window.location.href.replace('https://www.youtube.com/watch?v=', 'https://music.youtube.com/watch?v='), "_blank");
+    btn.innerText = "Open in YouTube Music";
+    btn.setAttribute("id", "jda-music-button");
+    btn.addEventListener("click", function() {
+      window.open(
+        window.location.href.replace(
+          "https://www.youtube.com/watch?v=",
+          "https://music.youtube.com/watch?v="
+        ),
+        "_blank"
+      );
     });
 
     var element = document.getElementById("jda-music-button");
     // Eliminar el elemento
     if (element) {
-        element.remove();
+      element.remove();
     }
 
+    const isMusic = document.querySelector(
+      "#items > ytd-video-description-music-section-renderer"
+    );
 
-    var searchbox = document.querySelector('ytd-searchbox')
-    console.log(searchbox)
-    if (searchbox) {
+    if (window.location.href != "https://www.youtube.com/" && isMusic != null) {
+      var searchbox = document.querySelector("ytd-searchbox");
+      if (searchbox) {
         searchbox.appendChild(btn);
+      }
     }
+  }, 3000);
 }
